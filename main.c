705a1bbb5523908d6da4cac460d1d387e92b8d7f
Lara Sotelo <larassotelo@gmail.com>
2022-05-20 00:21:00 -0300

    Primer commit, se agrega las principales funciones

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nombreStruct {
+    int dato;
+    struct nombreStruct *siguiente;
+} Nodo;
+
+
+void agregarElemento();
+void imprimir();
+void obtenerLargoDeLaLista();
+void obtenerElemento();
+void eliminarElemento();
+
+int main() {
+
+    return 0;
+}
